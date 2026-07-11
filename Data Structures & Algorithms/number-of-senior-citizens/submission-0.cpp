702a069e0cc786395ae8f19a9a8d46a2dc4052ba
chapalaumesh209 <chapalaumesh209@gmail.com>
2026-07-11 23:32:07 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int res=0;
+
+        int n = details.size();
+
+        for(int i=0;i<n;i++)
+        { 
+            int age = (details[i][11] - '0') * 10 + (details[i][12] - '0');
+            if(age > 60)
+            {
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
